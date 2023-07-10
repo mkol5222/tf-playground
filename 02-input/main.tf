3df0133964693d0cca8e60844e76cc1d8f92a77d
mkol5222 <mkoldov@gmail.com>
2023-07-10 09:23:28 +0000

    secrets vars

--- 02-input/main.tf
+++ 02-input/main.tf
@@ -6,4 +6,21 @@ variable "name" {
 output "greeting" {
     value = "Hello ${var.name}"
 }
-  
+
+variable "person" {
+  type = string
+  description = "variable defined as GH user codespace secret TF_VAR_person"
+}
+
+variable "team" {
+  type = string
+  description = "variable defined as GH repo codespace secret TF_VAR_team"
+}
+
+output "person-secret" {
+  value = "person secret is ${var.person}"
+}
+
+output "team-secret" {
+  value = "team secret is ${var.team}"
+} 
