1784d33a42c6fcfd7099a4887ae387f05fc201bf
mkol5222 <mkoldov@gmail.com>
2023-07-10 09:33:54 +0000

    var inputs

--- 02-input/main.tf
+++ 02-input/main.tf
@@ -1,3 +1,5 @@
+
+# https://developer.hashicorp.com/terraform/language/values/variables#variable-definition-precedence
 variable "name" {
     type = string
     default = "Earth"
@@ -7,20 +9,22 @@ output "greeting" {
     value = "Hello ${var.name}"
 }
 
-variable "person" {
+# https://github.com/settings/codespaces
+variable "PERSON" {
   type = string
-  description = "variable defined as GH user codespace secret TF_VAR_person"
+  description = "variable defined as GH user codespace secret TF_VAR_PERSON"
 }
 
-variable "team" {
+# http://github.com/mkol5222/tf-playground/settings/secrets/codespaces
+variable "TEAM" {
   type = string
-  description = "variable defined as GH repo codespace secret TF_VAR_team"
+  description = "variable defined as GH repo codespace secret TF_VAR_TEAM"
 }
 
 output "person-secret" {
-  value = "person secret is ${var.person}"
+  value = "person secret is ${var.PERSON}"
 }
 
 output "team-secret" {
-  value = "team secret is ${var.team}"
+  value = "team secret is ${var.TEAM}"
 } 
