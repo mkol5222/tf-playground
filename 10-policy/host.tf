626eab6ba5c1ad875eab0cf34f15dee34a00728e
mkol5222 <mkoldov@gmail.com>
2023-07-14 08:37:50 +0000

    .

--- 10-policy/host.tf
+++ 10-policy/host.tf
@@ -1,34 +1,34 @@
-resource "checkpoint_management_host" "example" {
-  name         = "martin-pol"
-  ipv4_address = "192.0.2.79"
-  color = "red"
-}
+# resource "checkpoint_management_host" "example" {
+#   name         = "martin-pol"
+#   ipv4_address = "192.0.2.79"
+#   color = "red"
+# }
 
-resource "checkpoint_management_host" "h1" {
-  name         = "h1"
-  ipv4_address = "192.0.2.201"
-  color = "cyan"
-}
+# resource "checkpoint_management_host" "h1" {
+#   name         = "h1"
+#   ipv4_address = "192.0.2.201"
+#   color = "cyan"
+# }
 
-resource "checkpoint_management_host" "h2" {
-  name         = "h2"
-  ipv4_address = "192.0.2.202"
-  color = "blue"
-}
+# resource "checkpoint_management_host" "h2" {
+#   name         = "h2"
+#   ipv4_address = "192.0.2.202"
+#   color = "green"
+# }
 
-resource "checkpoint_management_host" "h3" {
-  name         = "h3"
-  ipv4_address = "192.0.2.203"
-  color = "blue"
-}
+# resource "checkpoint_management_host" "h3" {
+#   name         = "h3"
+#   ipv4_address = "192.0.2.203"
+#   color = "blue"
+# }
 
-resource "checkpoint_management_host" "h4" {
-  name         = "h4"
-  ipv4_address = "192.0.2.204"
-  color = "blue"
-}
+# resource "checkpoint_management_host" "h4" {
+#   name         = "h4"
+#   ipv4_address = "192.0.2.204"
+#   color = "blue"
+# }
 
-import {
-    to = checkpoint_management_host.martin_import
-    id = "martin-import"
-}
+# import {
+#     to = checkpoint_management_host.martin_import
+#     id = "martin-import"
+# }
