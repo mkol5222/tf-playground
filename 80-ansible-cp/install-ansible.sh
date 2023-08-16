23d0f3db937701ff2b36918e3a91239f28a39043
mkol5222 <mkoldov@gmail.com>
2023-08-16 17:59:45 +0000

    ansible demo

---
+++ 80-ansible-cp/install-ansible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-ansible-on-ubuntu-20-04
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible -y
+
+
