adddda4fb8d1eac5df66e682fccda5bace94e429
mkol5222 <mkoldov@gmail.com>
2023-08-22 06:39:31 +0000

    d9 k8s config

--- 20-dome9/kubernetes.tf
+++ 20-dome9/kubernetes.tf
@@ -10,11 +10,11 @@
 # }
 
 resource "dome9_cloudaccount_kubernetes" "test" {
-  name  = "cluster-aug22"
+  name  = "cluster-aug22b"
   organizational_unit_id = "00000000-0000-0000-0000-000000000000"
-  runtime_protection {
-    enabled = false
-  }
+  # runtime_protection {
+  #   enabled = false
+  # }
   admission_control {
     enabled = true
   }
@@ -31,5 +31,13 @@ output "clusterId" {
 }
 
 output "helmcmd" {
-    value = "helm install asset-mgmt cloudguard --repo https://raw.githubusercontent.com/CheckPointSW/charts/master/repository/ --set credentials.user=\"${var.access_id}\" --set credentials.secret=${var.secret_key} --set clusterID=${dome9_cloudaccount_kubernetes.test.id} --set addons.flowLogs.enabled=false --set addons.runtimeProtection.enabled=false --set addons.imageScan.enabled=true --set addons.admissionControl.enabled=true --set datacenter=euwe1 --set imageRegistry.user=\"checkpoint+consec_read\" --set imageRegistry.password=V08FPKKJSHP8YJYLE571MMLAHPOSPX1ASFBI4P875L4ZNQWEXUCEU0V4ASWCZVAZ --namespace cp --create-namespace"
+    value = "helm install asset-mgmt cloudguard --repo https://raw.githubusercontent.com/CheckPointSW/charts/master/repository/ --set credentials.user=\"${var.D9K8SCLIENT}\" --set credentials.secret=${var.D9K8SSECRET} --set clusterID=${dome9_cloudaccount_kubernetes.test.id} --set addons.flowLogs.enabled=false --set addons.runtimeProtection.enabled=false --set addons.imageScan.enabled=true --set addons.admissionControl.enabled=true --set datacenter=euwe1 --set imageRegistry.user=\"checkpoint+consec_read\" --set imageRegistry.password=V08FPKKJSHP8YJYLE571MMLAHPOSPX1ASFBI4P875L4ZNQWEXUCEU0V4ASWCZVAZ --namespace cp --create-namespace"
+}
+
+variable "D9K8SCLIENT" {
+  
+}
+
+variable "D9K8SSECRET" {
+  
 }
